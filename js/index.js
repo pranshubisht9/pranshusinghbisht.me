37db974ef7b3621bb02affdcd547947ae43130b5
pranshubisht9 <pranshubisht9@gmail.com>
2022-11-04 23:56:32 +0530

    adding files

---
+++ js/index.js
@@ -0,0 +1,20 @@
+document.getElementById("sangharsh-official").addEventListener("click", function(){
+    window.open("https://www.youtube.com/channel/UCOYvT9NgBowQjzMfu8ptmkw");
+})
+
+
+document.getElementById("psb_online").addEventListener("click", function(){
+    window.open("https://www.pranshusinghbisht.online/");
+})
+let header_namaste = document.getElementById("namaste");
+header_namaste.addEventListener("click", function(){
+    window.location.reload();
+})
+header_namaste.style.cursor="pointer";
+
+
+document.getElementById("redirectReload").addEventListener("click", function(){
+    window.location.reload();
+})
+document.getElementById("redirectReload").style.cursor="pointer";
+document.getElementById("redirectReload").style.color="white";
